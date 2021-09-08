3258fadea1b57e0e749fbc5e844723edff48103b
Timothy Copley <alexmcguire2000@gmail.com>
2021-09-07 23:09:16 -0400

    dynamically added the div to hold the hour, textarea, and the save button. Added icon to save button and initialized the display array inside the for loop. added the classes and data attr's to the respective variables, next will add functionality

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,29 @@ var displayArray = [9,10,11,12,13,14,15,16,17]
 
 for (i = 0; i < displayArray.length; i++) {
 
+    var timeBlockEl = $("<div>");
+    timeBlockEl.addClass("time-block");
+
+    var hour = displayArray[i];
+
+    var timeSpanEl = $("<span>");
+    timeSpanEl.addClass("hour");
+
+    var textAreaEl = $("<textarea>");
+    textAreaEl.addClass("description");
+    
+    var saveButtonEl = $("<button>");
+    saveButtonEl.addClass("saveBtn");
+    saveButtonEl.data("time", displayArray[i]);
+    saveButtonEl.append($('<i class="far fa-save"></i>'))
+
+    timeBlockEl.append(timeSpanEl, textAreaEl, saveButtonEl);
+    timeBlocks.append(timeBlockEl);
+
 }
+
+
+
 
 function displayTime() {
     var rightNow = moment().format("dddd, MMMM Do");
