36f42434ba6b49108160e082224a927a4d1499de
Timothy Copley <alexmcguire2000@gmail.com>
2021-09-07 23:17:24 -0400

    added function to the loop for displaying the hours, added boostrap grid  to allow correct spacing. Added the localstorage set and get to display properly. Finished project

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,56 @@ var timeBlocks = $("#timeBlocks");
 
 var displayArray = [9,10,11,12,13,14,15,16,17]
 
-
 for (i = 0; i < displayArray.length; i++) {
 
+    var savedDescription = localStorage.getItem(displayArray[i]);
     var timeBlockEl = $("<div>");
-    timeBlockEl.addClass("time-block");
-
-    var hour = displayArray[i];
+    timeBlockEl.addClass("time-block row")
+    
+    var hour = displayArray[i]
 
     var timeSpanEl = $("<span>");
-    timeSpanEl.addClass("hour");
+    timeSpanEl.addClass("col hour")
+    if (hour > 12) {
+        timeSpanEl.text((hour - 12) + "PM");
+    } else {
+        timeSpanEl.text(hour + "AM");
+    }
 
     var textAreaEl = $("<textarea>");
-    textAreaEl.addClass("description");
-    
+    textAreaEl.addClass("description col-10");
+    textAreaEl.attr("data-gramm", "false");
+    if (hour < moment().hours()) {
+        textAreaEl.addClass("past");
+    } else if (hour === moment().hours()){
+        textAreaEl.addClass("present");
+    } else {
+        textAreaEl.addClass("future");
+    }
+
+    if (savedDescription) {
+    textAreaEl.val(savedDescription);
+    }
+
     var saveButtonEl = $("<button>");
-    saveButtonEl.addClass("saveBtn");
+    saveButtonEl.addClass("saveBtn col");
     saveButtonEl.data("time", displayArray[i]);
     saveButtonEl.append($('<i class="far fa-save"></i>'))
 
     timeBlockEl.append(timeSpanEl, textAreaEl, saveButtonEl);
     timeBlocks.append(timeBlockEl);
 
-    
 }
 
+$(".saveBtn").on("click", function() {
+    var time = $(this).data("time");
+    var description = $(this).prev().val();
 
-
+    localStorage.setItem(time, description);
+});
 
 function displayTime() {
     var rightNow = moment().format("dddd, MMMM Do");
     todaysDate.text(rightNow);
-}
-
+};
 displayTime();
