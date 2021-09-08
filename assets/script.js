e8d32f47a013082edb0a0d63b25abb9d2e0108ac
Timothy Copley <alexmcguire2000@gmail.com>
2021-09-07 23:01:03 -0400

    added variables to call id's from the html, added initial for loop to later on dynamically create the planner, also added the time to the top to show the current time.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var todaysDate = $("#currentDay");
+var timeBlocks = $("#timeBlocks");
+
+var displayArray = [9,10,11,12,13,14,15,16,17]
+
+
+for (i = 0; i < displayArray.length; i++) {
+
+}
+
+function displayTime() {
+    var rightNow = moment().format("dddd, MMMM Do");
+    todaysDate.text(rightNow);
+}
